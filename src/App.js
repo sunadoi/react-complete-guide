170b83d343cb93af96b97977122265d14b6fcc64
sunadoi <y.sunadoi@gmail.com>
2020-04-21 08:28:22 +0900

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+
 import Person from './Person/Person.js';
 
 class App extends Component {
@@ -91,19 +91,17 @@ class App extends Component {
     }
   
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-            {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
 
