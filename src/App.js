961780c75d6af50fd472935859b20a5168740a95
sunadoi <y.sunadoi@gmail.com>
2020-04-21 07:35:10 +0900

    radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person.js';
 
 class App extends Component {
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -94,5 +103,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
