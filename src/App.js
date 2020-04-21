aa9abeef63a70337a2e0161cab476287c473a279
sunadoi <y.sunadoi@gmail.com>
2020-04-22 07:24:09 +0900

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person.js';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js'
 
 class App extends Component {
   state = {
@@ -52,12 +53,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+               />
+            </ErrorBoundary>
           })}
         </div>
       )
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,12 +3,18 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+  
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+      throw new Error('Something went wrong');
+    }
+    return (
+      <div className={classes.Person}>
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
+      </div>
   )
 }
 
