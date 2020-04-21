21d77b9eba7c4d0f30248df5bddfec7c747be854
sunadoi <y.sunadoi@gmail.com>
2020-04-22 08:11:21 +0900

    devide components to Persons and Cockpit

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+  const assignedClasses = [];
+  let btnClass = [classes.Button];
+  if (props.showPersons) {
+    btnClass.push(classes.Red);
+  }
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red); // assignedClasses = ['red'];
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold'];
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hi, I'm a React App</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button className={btnClass.join(' ')} onClick={props.clicked}>Toggle Persons</button>
+    </div>
+  );
+};
+
+export default cockpit;
